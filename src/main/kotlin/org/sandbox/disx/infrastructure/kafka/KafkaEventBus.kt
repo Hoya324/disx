c1ca7fcbc9ddfb3c440b10beb40625441fdff095
Hoya324 <hoyana1225@gmail.com>
2025-06-28 21:33:06 +0900

    DS-03 feat: event bus with kafka

---
+++ src/main/kotlin/org/sandbox/disx/infrastructure/kafka/KafkaEventBus.kt
@@ -0,0 +1,28 @@
+package org.sandbox.disx.infrastructure.kafka
+
+import org.sandbox.disx.core.events.BaseDomainEvent
+import org.sandbox.disx.core.events.EventBus
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class KafkaEventBus : EventBus {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun <T : BaseDomainEvent> publish(event: T) {
+        logger.debug("🔄 Event publish intercepted by Outbox pattern: ${event.eventId}")
+    }
+
+    override suspend fun <T : BaseDomainEvent> publishAsync(event: T) {
+        publish(event)
+    }
+
+    override fun <T : BaseDomainEvent> publishBatch(events: List<T>) {
+        events.forEach { publish(it) }
+    }
+
+    override suspend fun <T : BaseDomainEvent> publishBatchAsync(events: List<T>) {
+        publishBatch(events)
+    }
+}
